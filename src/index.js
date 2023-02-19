77bcab483f10e5967cf2d9335530c97973356a74
Voitko Viacheslav <voitko-viacheslav-ivanovich1992@ukr.net>
2023-02-19 17:48:27 +0200

    feat: create Morse Decoder

--- src/index.js
+++ src/index.js
@@ -37,10 +37,56 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
-function decode(expr) {
-    // write your solution here
+function decode(expr) { 
+
+    let array = [];
+    for (let i = 0; i < expr.length; i+=10) {
+        let str = '';
+        str +=(expr.slice(i, i+10));
+
+        while(str.length < 10) {
+            str = '0' + str;
+        }
+        array.push(str)
+    }
+
+    let result = [];
+    array.forEach(item => {
+        let res = [];
+
+        for (let i = 0; i < 10; i+=2) {
+            let str = item.slice(i, i+2);
+            if (str == 10) {
+                res.push('.') 
+            } 
+            else if
+            (str == 11) {
+                res.push('-')  
+            }
+            else if
+            (str == '') {
+                res.push(' ')  
+            }
+        }
+
+        let current = res.join('');
+        for (key in MORSE_TABLE) {
+            if (key == current) {
+                result.push(MORSE_TABLE[key])
+            }   
+        } 
+
+        if (current == '') {
+            result.push(' ');
+        }
+    })
+
+    return result.join('');
 }
+console.log(decode('0010101010000000001000101110100010111010000011111100001011110000111111000010111000101110100000111010'));
 
 module.exports = {
     decode
 }
+
+
